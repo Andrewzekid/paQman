e9b4f27d4b98a663d6e8206fe8318d07e3516743
Legend Andre <andreportilla@gmail.com>
2023-12-23 11:46:13 +0900

    Added data

--- score.py
+++ score.py
@@ -2,12 +2,14 @@ import numpy as np
 import pyautogui
 import imutils
 import cv2
+import keyboard
 import time
 from matplotlib import pyplot as plt
 import pytesseract
 from pytesseract import image_to_string
+import os
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
+DATA_PATH = "data"
 def locate_score_bar():
     #Start the game
     play_button = pyautogui.locateCenterOnScreen("play.png")
@@ -25,13 +27,21 @@ def locate_score_bar():
     return left,top,width,height
 
 
-def take_screenshot(left,top,width,height):
+def create_training_data(left,top,width,height):
     """Given the top left corner of the scorebar, the width and its height, return a screenshot of the scorebar"""
     print("Do your moves!")
-    time.sleep(20)
-
     score_tlbr = (left-width*2,top,width*6,height) #road to 20kws
-    image = pyautogui.screenshot("score.png",region=score_tlbr)
+    time.sleep(30)
+    for i in range(500,2000):
+        filepath = fr"c:\Users\yw347\Downloads\Project +1\data\score{i}.png"
+        image = pyautogui.screenshot(filepath,region=score_tlbr)
+        print(f"image with filepath {filepath} saved!")
+        if keyboard.is_pressed("q"):
+            break
+
+
+    
+    
     image = cv2.cvtColor(np.array(image),cv2.COLOR_BGR2GRAY)
     return image
 
@@ -45,6 +55,7 @@ def calculate_score(img):
     return txt
 
 
+
 if __name__ == "__main__":
     #Test run
     time.sleep(5)
@@ -52,5 +63,5 @@ if __name__ == "__main__":
     l,t,w,h = locate_score_bar()
     time.sleep(5)
     print("tracking score!")
-    image = take_screenshot(l,t,w,h)
+    create_training_data(l,t,w,h)
     print(calculate_score(image))
